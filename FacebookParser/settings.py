6e06fa92d751f534e74e23267f211e78686c4957
Hordii Rushynets <hordii.rushynets@synapse.com>
2020-02-17 19:07:06 +0200

    added templates

--- FacebookParser/settings.py
+++ FacebookParser/settings.py
@@ -54,20 +54,19 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'FacebookParser.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [os.path.join(BASE_DIR, 'templates')],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
     },
-]
+},]
 
 WSGI_APPLICATION = 'FacebookParser.wsgi.application'
 
