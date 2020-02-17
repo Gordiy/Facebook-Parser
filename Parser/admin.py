9b95476de79eb95331b544d69e526fbbe10bebec
Hordii Rushynets <hordii.rushynets@synapse.com>
2020-02-17 18:07:00 +0200

    created Follower and Group Model

--- Parser/admin.py
+++ Parser/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Follower, Group
 
-# Register your models here.
+
+admin.site.register(Group)
+admin.site.register(Follower)
--- Parser/models.py
+++ Parser/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    link = models.TextField()
+
+    def __str__(self):
+        return 'Group link: {0}'.format(self.link)
+
+class Follower(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    fb_id = models.CharField(max_length=25)
+
+    def __str__(self):
+        return 'Follower ID: {0}, {1}'.format(self.fb_id, self.group)
+
